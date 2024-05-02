b85792361605698a28f9e49500b7a71cc1334dc5
Atul Chaudhary <133472429+atulchoudhary123276@users.noreply.github.com>
2024-05-02 19:56:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Resturant from './Component/Basics/Resturant';
+// import UseState from './Component/Hooks/useState';
+// import UseEffect from './Component/Hooks/useEffect';
+// import UseReducer from './Component/Hooks/useReducer';
+
+const App = () => {
+  return <Resturant/>;
+  
+}
+
+export default App;
---
+++ src/Component/Basics/Resturant.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import "./style.css";
+import Menu from "./menuApi";  // comes data
+import MenuCard from './MenuCard';  //it is component which get data from Menu
+import Navbar from './Navbar';
+
+
+const uniqueList = [
+  ...new Set(Menu.map((curElem) => {
+    return curElem.category;
+  })
+  ),
+  "All",
+];
+console.log(uniqueList);
+
+const Resturant = () => {
+  // --hooks first data on top  -it is use to getting data from Menu(API)
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+    const updateList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+    setMenuData(updateList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      {/* use props for getting data(menuData) in loop and set it on component (MenuCard)  */}
+      <MenuCard menuData={menuData} />
+    </>
+  );
+};
+
+export default Resturant;
